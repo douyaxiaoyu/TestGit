4bbcb95853acd7ce46aead3e1e801ff7c0116f2f
eclipse_user <eclipse_user@163.com>
2018-09-07 00:05:34 +0800

    ddd

--- src/main/java/cn/hpe/git/Man.java
+++ src/main/java/cn/hpe/git/Man.java
@@ -3,7 +3,7 @@ package cn.hpe.git;
 public class Man {
 
 	public static void main(String[] args) {
-		System.out.println("abc");
+		System.out.println("left");
 		System.out.println("abc");
 		System.out.println("abc");
 		System.out.println("abc");
